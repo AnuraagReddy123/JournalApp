dfc7cb8168de571cdade3b0a18691b302d299bdc
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-07-27 12:38:37 +0530

    Added Drawables

--- app/src/main/java/com/blueticks/journal/MainActivity.java
+++ app/src/main/java/com/blueticks/journal/MainActivity.java
@@ -3,12 +3,23 @@ package com.blueticks.journal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.start_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Hello", "onClick: clicked");
+            }
+        });
     }
 }
