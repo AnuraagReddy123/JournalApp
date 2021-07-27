dc088777ec6abbe7f1c27282096641e2ba18e1e3
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-07-27 13:01:53 +0530

    Minor Changes

--- app/src/main/java/com/blueticks/journal/MainActivity.java
+++ app/src/main/java/com/blueticks/journal/MainActivity.java
@@ -2,23 +2,25 @@ package com.blueticks.journal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b;
+    private Button getStartedButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b = findViewById(R.id.start_button);
-        b.setOnClickListener(new View.OnClickListener() {
+        getStartedButton = findViewById(R.id.start_button);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Hello", "onClick: clicked");
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
     }
