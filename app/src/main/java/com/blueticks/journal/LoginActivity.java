dda6a13d489ecfa05e0bd6d45d3580c0a051105a
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-07-28 12:27:12 +0530

    Added create account page

--- app/src/main/java/com/blueticks/journal/LoginActivity.java
+++ app/src/main/java/com/blueticks/journal/LoginActivity.java
@@ -2,13 +2,35 @@ package com.blueticks.journal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button loginButton;
+    Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createButton = findViewById(R.id.create_acct_button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+            }
+        });
     }
 }
