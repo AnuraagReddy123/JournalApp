0af9291d255bafee5cecf7e462ee945bf47a5222
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-08-04 23:41:03 +0530

    Added Menu

--- app/src/main/java/com/blueticks/journal/JournalListActivity.java
+++ app/src/main/java/com/blueticks/journal/JournalListActivity.java
@@ -1,8 +1,11 @@
 package com.blueticks.journal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class JournalListActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class JournalListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                // Take Users to add Journal
+                break;
+            case R.id.action_signout:
+                // Signout
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
