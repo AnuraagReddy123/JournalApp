8189daccaa176b53c3a977292a6a9e1f245e67fd
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-08-04 12:51:29 +0530

    some more changes before login

--- app/src/main/java/com/blueticks/journal/LoginActivity.java
+++ app/src/main/java/com/blueticks/journal/LoginActivity.java
@@ -1,22 +1,43 @@
 package com.blueticks.journal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
     private Button loginButton;
     private Button createButton;
     private AutoCompleteTextView emailEditText;
     private EditText passwordEditText;
     private ProgressBar progressBar;
+    private LinearLayout loginForm;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +54,7 @@ public class LoginActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.email);
         passwordEditText = findViewById(R.id.password);
         progressBar = findViewById(R.id.login_progress);
+        loginForm = findViewById(R.id.email_login_form);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +73,23 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUserWithEmailPassword(String email, String password) {
-        
+        progressBar.setVisibility(View.VISIBLE);
+        loginForm.setVisibility(View.GONE);
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            currentUser = firebaseAuth.getCurrentUser();
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+                            loginForm.setVisibility(View.VISIBLE);
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
